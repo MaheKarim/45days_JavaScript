288b5caadedc2b2bd6a8b329cef8dedd75a75d72
mahekarim <mahekarim@gmail.com>
2021-05-03 21:48:13 +0600

    Array Example

---
+++ 1st_Day/array.js
@@ -0,0 +1,16 @@
+
+const profile = ['Mahe', 'Karim', '25', 'Software Engineer'];
+
+//console.log(profile);
+
+profile.push('4', 'Check');
+
+// console.log(profile);
+
+profile.pop('3', '4'); // This is wrong method : TODO splice(1, 3)
+
+console.log(profile)
+
+profile.pop();
+
+console.log(profile)
