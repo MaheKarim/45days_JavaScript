34cddab9a24764fc4832930cb5d73dc7ae1e8876
mahekarim <mahekarim@gmail.com>
2021-05-03 19:31:03 +0600

    Template String

--- 1st_Day/main.js
+++ 1st_Day/main.js
@@ -1,1 +1,17 @@
 console.log(1+1);
+//const, let , var
+const productName = 'potato';
+const productPrice = 30;
+const productQuantity = 3;
+const productIsAvailable = 'true';
+
+console.log(`
+   productName -
+       ${productName}
+     productPrice- 
+        ${productPrice}
+    quantity 
+     ${productQuantity}
+    isAvailable  
+     ${productIsAvailable} 
+     `);
