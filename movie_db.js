1b516eb382dc4cd481708f8a208b1b8f5e62e9c0
mahekarim <mahekarim@gmail.com>
2021-05-05 21:23:48 +0600

    Movie DB

---
+++ movie_db.js
@@ -0,0 +1,19 @@
+
+
+const movie1 = 
+    {
+        id: 1,
+        name: 'BodyGuard',
+        rating: 4.6,
+        category: 'Horror',
+    }
+
+    const movie2 = 
+    {
+        id: 2,
+        name: 'Munna Bhai',
+        rating: 4.3,
+        category: 'Comedy',
+    }
+
+    console.log(movie1);
