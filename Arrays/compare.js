7b67e717a666d44df396fdcce4553043ce067b3e
mahekarim <mahekarim@gmail.com>
2021-05-21 16:18:32 +0600

    Update

--- Arrays/compare.js
+++ Arrays/compare.js
@@ -3,6 +3,21 @@ var arr = [
     { name: 'XYZ' },
     { name: 'BBC' },
 ];
+
+var checkMyObj = [
+    {   x: 23,
+        y: 34
+    },
+    {
+        x:16,
+        y: 88
+    }];
+console.log(checkMyObj);
+checkMyObj.sort();
+console.log(checkMyObj)
+
+
+
 function compareName(a,b) { // Actually How This Arguments Are Working
     return a.name.localeCompare(b.name);
 }
