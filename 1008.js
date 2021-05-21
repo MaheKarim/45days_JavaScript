b2eb912a0bd6a80e3d602dd870b715785db4326d
mahekarim <mahekarim@gmail.com>
2021-05-21 23:32:10 +0600

    1008 Solvee

--- 1008.js
+++ 1008.js
@@ -1,13 +1,11 @@
 
-const employeeObj = {
-    employeNumber: 25,
-    totalHour: 100,
-    perHourSalaray: 5.50
-}
- function totalCalculation(employeeObj) {
-     var myAnswer = employeeObj.totalHour * employeeObj.perHourSalaray;
-     console.log(`NUMBER = ${employeeObj.employeNumber}`)
-     console.log(`SALARY = U$ ${myAnswer.toFixed(2)}`);
- }
- const functionResult = totalCalculation(employeeObj);
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
 
+var idEmployee = lines.shift();
+var workedHours = lines.shift();
+var receivedHour = lines.shift();
+
+var salary = workedHours * receivedHour ;
+console.log("NUMBER = " + idEmployee);
+console.log("SALARAY = U$ " + salary.toFixed(2));
