8f78810bf8cd195e0f2c5b3dc394a1fc351ba00a
mahekarim <mahekarim@gmail.com>
2021-05-23 00:19:27 +0600

    Find The Square Value Of Every Element

---
+++ 10 DAYS JS/getEverySqureArrayElement.js
@@ -0,0 +1,11 @@
+
+
+
+    var arr = [1, 2, 3, 5, 4 ,6 , 7, 8, 9]
+    var anotherArr = [];
+
+for (var i = 0; i < arr.length; i++){ // Read Every Element From arr[]
+    anotherArr.push(arr[i] * arr[i]); // Push Arry To anotherArr as arr * arr
+}
+console.log(anotherArr) // Finally Publish Result
+
