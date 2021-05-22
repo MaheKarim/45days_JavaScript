189f5c49671e08607a81f029ffa02798aa37c2c0
mahekarim <mahekarim@gmail.com>
2021-05-23 00:57:03 +0600

    1 Line Solution

--- Arrays/sortArrayByStringLength.js
+++ Arrays/sortArrayByStringLength.js
@@ -5,4 +5,6 @@ function sortByLength (array) {
     // Return an array containing the same strings, ordered from shortest to longest
     return [...array].sort((a,b) => a.length-b.length);
 };
+// 1 Line Solution
+let sortByLength = arr => arr.sort((a,b)=> a.length-b.length)
 
